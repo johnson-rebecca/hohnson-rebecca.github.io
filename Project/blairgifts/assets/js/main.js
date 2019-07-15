6a0a992fc5bf6901e255d3eaf54f6fc5c3ee1a39
johnson-rebecca <49816710+johnson-rebecca@users.noreply.github.com>
2019-07-14 21:52:12 -0400

    Add files via upload

---
+++ Project/blairgifts/assets/js/main.js
@@ -0,0 +1,15 @@
+// main page JS - simply-nav does not need this
+(pageDuty => {
+  const on = '<strong>On</strong>';
+  const off = '<strong>Off</strong>';
+  const navWrapper = document.querySelector('.nav-wrapper');
+  const mainWrapper = document.querySelector('main');
+  const burgerIcon = document.querySelector('.burger');
+
+
+//  menuToggle.addEventListener('click', () => {
+//    burgerIcon.classList.toggle('-offset');
+//    (burgerIcon.classList.contains('-offset') ? menuText.innerHTML = off : menuText.innerHTML = on);
+//  });
+
+})();
